9a2a9ac1400c8bf68d8e6e0e64442f3dca6f16e8
유동수 <badongsch3@gmail.com>
2024-05-16 18:39:00 +0900

    페이지 매김 추가 부터 시작

--- nextjs-app2/app/ui/search.tsx
+++ nextjs-app2/app/ui/search.tsx
@@ -2,19 +2,32 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+
+import { useDebouncedCallback } from 'use-debounce';
 // export default function Search({ placeholder }: { placeholder: string }) {
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
+
   function handleSearch(term: string) {
-    const params = new URLSearchParams(searchParams);
-    if (term) {
-      params.set('search', term);
-    } else {
-      params.delete('search');
-    }
-    replace(`${pathname}?${params.toString()}`);
+    // Inside the Search Component...
+    //const handleSearch = useDebouncedCallback((term) => {
+
+    //}, 300);
+    let timeoutId;
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      console.log(`Searching... ${term}`);
+
+      const params = new URLSearchParams(searchParams);
+      if (term) {
+        params.set('query', term);
+      } else {
+        params.delete('query');
+      }
+      replace(`${pathname}?${params.toString()}`);
+    }, 5000);
   }
 
   return (
