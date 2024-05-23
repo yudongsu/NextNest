32f9b65c1dbe4b6962b23f988e4a7c558ffe786b
유동수 <badongsch@gmail.com>
2024-05-24 00:05:23 +0900

    인증추가 시작

--- nextjs-app2/app/ui/invoices/edit-form.tsx
+++ nextjs-app2/app/ui/invoices/edit-form.tsx
@@ -18,7 +18,9 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const initialState = { message: null, errors: {} };
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+  const [state, dispatch] = useFormState(updateInvoiceWithId, initialState);
   return (
     <form action={updateInvoiceWithId}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
